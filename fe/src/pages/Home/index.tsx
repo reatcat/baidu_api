aaec3f0fee2e47a496d4a9bf590053cf25a7aed1
zhoufu <2739904664@qq.com>
2023-05-19 20:30:24 +0800

    update: update home function

--- fe/src/pages/Home/index.tsx
+++ fe/src/pages/Home/index.tsx
@@ -1,10 +1,45 @@
 // 此页面为主页页面
 import { FC,useEffect,useState } from "react"
 import { Link, useNavigate } from "react-router-dom"
+import {ExclamationCircleOutlined} from '@ant-design/icons'
+import { Modal, message,Popover } from 'antd'
 import axios from "axios"
 const Home:FC = ()=>{
     const [username,setUsername] = useState('')
     const [log,setLog] = useState(false)
+    const [open,setOpen] = useState(false)
+    // 定义退出modal
+    const [modal_logout, contextHolder] = Modal.useModal()
+    // 定义注销modal
+    const [modal_unregister, contextHolders] = Modal.useModal()
+    // showmodal函数
+    // 退出登录，应弹出窗口提示,确认后退出
+    const showmodal_logout = () => {
+        modal_logout.confirm({
+        title: '警告',
+        icon: <ExclamationCircleOutlined />,
+        content: '确认退出登录吗',
+        okText: '确认',
+        cancelText: '取消',
+        onOk:()=>{
+            logout()
+        },
+        })
+    }
+    // 注销账户，应弹出窗口提示,确认后注销
+    const showmodal_unregister = () => {
+        setOpen(false)
+        modal_unregister.confirm({
+        title: '警告',
+        icon: <ExclamationCircleOutlined />,
+        content: '确认注销该账户吗,注销后该账户不再存在!',
+        okText: '确认',
+        cancelText: '取消',
+        onOk:()=>{
+            unregister()
+        },
+        })
+    }
     // 使用axios获得用户名
     useEffect(()=>{
         axios.get('/api/user/index')
@@ -12,16 +47,41 @@ const Home:FC = ()=>{
             // 依据返回的code确定三个状态
             const code = res.data.data.code
             if(code === 1){
-            const name = res.data.data.username
-            // 设置用户名
-            setUsername(name)
-            setLog(true)
-            console.log(username,log)
+                const name = res.data.data.username
+                // 设置用户名
+                setUsername(name)
+                setLog(true)
             }
         })
     },[])
+    // 注销函数
+    const unregister = ()=>{
+        axios.post('/api/user/unregister')
+        .then(()=>{
+            setLog(false)
+            setUsername('')
+        })
+    }
+    // 退出函数
+    const logout = () =>{
+        axios.post('/api/user/logout')
+        .then(()=>{
+            setLog(false)
+            setUsername('')
+        })
+    }
     return (
         <div>
+            {
+                log?
+                <div>
+                    周富
+                </div>
+                :
+                <div>
+                    周福
+                </div>
+            }
             啦啦啦啦
             <Link to="/Login" >
                 登录
