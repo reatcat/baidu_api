28ef82200725ba0846fafada5387101d2579b00d
zhoufu <2739904664@qq.com>
2023-05-19 20:12:34 +0800

    update: update home

--- fe/src/pages/Home/index.tsx
+++ fe/src/pages/Home/index.tsx
@@ -1,7 +1,25 @@
 // 此页面为主页页面
-import { FC } from "react"
+import { FC,useEffect,useState } from "react"
 import { Link, useNavigate } from "react-router-dom"
+import axios from "axios"
 const Home:FC = ()=>{
+    const [username,setUsername] = useState('')
+    const [log,setLog] = useState(false)
+    // 使用axios获得用户名
+    useEffect(()=>{
+        axios.get('/api/user/index')
+        .then((res)=>{
+            // 依据返回的code确定三个状态
+            const code = res.data.data.code
+            if(code === 1){
+            const name = res.data.data.username
+            // 设置用户名
+            setUsername(name)
+            setLog(true)
+            console.log(username,log)
+            }
+        })
+    },[])
     return (
         <div>
             啦啦啦啦
